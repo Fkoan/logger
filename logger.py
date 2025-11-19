ccef3fe95a83d6131ba9ccff13491b4b8c7587dd
Fkoan <150195647+Fkoan@users.noreply.github.com>
2025-11-19 09:59:57 +0100

    Add files via upload

---
+++ logger.py
@@ -0,0 +1,56 @@
+from tkinter import *
+from tkinter import ttk,messagebox
+
+
+class Logger:
+	def __init__(self,root):
+		self.root = root
+		self.root.title("Logger")
+		self.root.geometry("400x400")
+		self.root.config(bg="#c0c0c0")
+		#~ self.root.config(bg="#d2691e")
+		self.root.resizable(width="false",height="false")
+		try:
+			self.root.iconbitmap("pics/my_nick_name.ico")
+		except:
+			pass
+		self.front_title()
+
+	def front_title(self):
+		#~ this is for the title box
+		self.frame1 = Frame(root, bg="#a52a2a")
+		self.frame1.grid(row=0, column=0, columnspan=100, padx=150, pady=5)
+		self.title_box = Label(self.frame1, text="LOGGER",bg="#a52a2a", font=("Arial", 21, "bold"))
+		self.title_box.grid(row=0, column=0, pady=10)
+		self.student_details()
+
+	def student_details(self):
+		#~ this handles student's details
+		self.matric_num_label = Label(self.root, text="Matric no", font=("Arial", 20, "bold"))
+		self.matric_num_label.grid(row=1, column=0, padx=(5,0), pady=15)
+		self.matric_num_entry = Entry(self.root)
+		self.matric_num_entry.grid(row=1, column=1, padx=(10,0))
+		self.price_label = Label(self.root, text="price", font=("Arial", 20, "bold"))
+		self.price_label.grid(row=2, column=0, pady=10)
+		self.price_entry = Entry(self.root)
+		self.price_entry.grid(row=2, column=1)
+		self.log_btn = Button(self.root, text="LOG", font=("Arial", 10, "bold"),command=self.log_details)
+		self.log_btn.grid(row=3, column=1)
+
+	def log_details(self):
+		#~ this handles and check if the details are valid or not
+		try:
+			self.mat_get = self.matric_num_entry.get()
+			self.price_get = int(self.price_entry.get())
+			if self.mat_get and self.price_get:			
+				self.matric_num_entry.delete(0,END)
+				self.price_entry.delete(0,END)
+				messagebox.showinfo("Info","Log successful")
+			
+		except:
+			messagebox.showerror("info","try again!")
+
+if __name__ == "__main__":
+	root = Tk()
+	App = Logger(root)
+	root.mainloop()
